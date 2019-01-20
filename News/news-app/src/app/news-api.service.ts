a9d1b4861601d2d1f7724997d53b9f987523d393
bhanupal123 <bhanupalsingh@gmail.com>
2019-01-20 15:49:40 +0530

    nodeplus react

---
+++ News/news-app/src/app/news-api.service.ts
@@ -0,0 +1,23 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = '9b7cf65a94624b69ad173bc8dfaa274a';
+
+  constructor(private http:HttpClient) { }
+  initSources(){
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+} 
